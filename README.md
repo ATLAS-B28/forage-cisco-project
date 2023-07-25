e0541b93c219c2b8873c13b209a415adf9913935
ATLAS-B28 <adityacbhambere28@gmail.com>
2023-07-25 17:55:51 +0530

    Added more description

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # forage-cisco-project
-This project was developed as part of the Cisco Intro to Engineering Virtual Experience Program
+This project was developed as part of the Cisco Intro to Engineering Virtual Experience Program.
+Tools - 
+1) React.JS-18
+2) Vite Build Tool
+3) Websokect NPM Package
+4) Git and GitHub
+5) Node.JS-20
--- sextant/src/App.js
+++ sextant/src/App.js
@@ -8,7 +8,9 @@ function App(props) {
   return (
     <div className="App">
       <Banner head="Sextant"/>
-      <p>Sextant's purpose is to brings ease and verstality at your finger tips and allows developers, network engineers to get the IP addresses of client's servers and get the latency of the servers .It aids in better opertionality for clients and ease for maintainence teams.</p>
+      <p>Sextant's purpose is to brings ease and verstality at your finger tips and allows developers, network engineers to get the IP 
+        addresses of client's servers and get the latency of the servers .It aids in better opertionality for clients and ease for 
+        maintainence teams. Build by using React.JS</p>
       <Card name="Public IPv4 Address">
         <Display url="https://api.ipify.org?format=json"/>
       </Card>
